969f3aa481ab31b495e3ea28f26450f89238016c
Navaneeth B K <160996953+Navan-1998@users.noreply.github.com>
2024-09-11 23:34:58 +0530

    Add files via upload

---
+++ 2.Print Even num upto Input.cs
@@ -0,0 +1,29 @@
+﻿//Print even numbers upto user input
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Print_Even_Numbers_Upto_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Console.WriteLine("Enter Upto Which Even Numbers are Needed:");
+            input = Convert.ToInt32(Console.ReadLine());
+            int a;
+            Console.WriteLine($"Even Numbers upto {input} is:");
+            for (int i = 1;i<=input;i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
