f651db1d3e8e5d34c57624e80a7c2f73f5efe6bc
Navaneeth B K <160996953+Navan-1998@users.noreply.github.com>
2024-09-12 01:43:00 +0530

    Add files via upload

---
+++ Start Traingle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace startriangle_Printing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter Number of Lines of * Needed:");
+            n=Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
