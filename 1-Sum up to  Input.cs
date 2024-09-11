7ee994c2b2ea8a41b3b54538ec9a042094e67329
Navaneeth B K <160996953+Navan-1998@users.noreply.github.com>
2024-09-11 23:00:54 +0530

    Add files via upload

---
+++ 1-Sum up to  Input.cs
@@ -0,0 +1,26 @@
+﻿//1.Sum of Numbers upto User Input
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            int sum = 0;
+            Console.WriteLine("Enter a Number Upto which addition is needed:");
+            input=Convert.ToInt32(Console.ReadLine());
+            for (int i=0;i<=input;i++)
+            {
+                sum=sum+i;
+            }
+            Console.WriteLine($"Sum upto the Number{input} is:{sum}");
+            Console.ReadKey();
+        }
+    }
+}
